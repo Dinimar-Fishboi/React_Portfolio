74e16457fe12e2654e0f530dc0a00f2b62be04e1
Dinimar Fish <tayla@tallisman.com.au>
2021-10-23 17:03:13 +1000

    set up base exports and imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Container from "./components/Container"
+import Container from "./components/Container";
 
 const App = () => <Container />;
 
