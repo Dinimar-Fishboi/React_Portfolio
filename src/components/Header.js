11963bc7756eae0444ac419e7a4c4cb9c83a6b0b
Dinimar Fish <tayla@tallisman.com.au>
2021-10-23 20:15:54 +1000

    removed anchor tag and confirmed page component rendered

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,55 +24,16 @@ function Header({ currentPage, handlePageChange, name }) {
                 (item) =>   {
                     console.log(item)
                 return (
-                    <li className="nav-item" key={item.index}>
-                    <a
-                    href={item}
-                    onClick={() => handlePageChange(item)}
-                    className={currentPage === item ? 'nav-link active' : 'nav-link'}
-                    >
-                    {item}
-                    </a>
-            </li>
-                )
-                }
-            )
-            }
-            {/* <li className="nav-item">
-                <a
-                href="#about"
-                onClick={() => handlePageChange('About')}
-                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                href="#portfolio"
-                onClick={() => handlePageChange('Portfolio')}
-                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                Portfolio
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                href="#contact"
-                onClick={() => handlePageChange('Contact')}
-                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                Contact
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                href="#resume"
-                onClick={() => handlePageChange('Resume')}
-                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                Resume
-                </a>
-            </li> */}
+                    <li className="nav-item" key={item.index}
+                        href={item}
+                        onClick={() => handlePageChange(item)}
+                        className={currentPage === item ? 'nav-link active' : 'nav-link'}>
+                        {item}
+                    </li>
+                      )
+                  }
+              )
+            }    
             </ul>
         </header>
     </>
