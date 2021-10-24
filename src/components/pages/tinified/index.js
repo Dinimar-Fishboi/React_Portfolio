19b29dc62071c3de6928e3e9f9e05de628368697
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 12:32:38 +1000

    added images to be used in Portfolio

---
+++ src/components/pages/tinified/index.js
@@ -0,0 +1,10 @@
+const imageFiles = {
+    barter: require('./Barter.png').default,
+    bored: require('./Bored.png').default,
+    budget: require('./Budget.png').default,
+    note: require('./Note.png').default,
+    password: require('./Password.png').default,
+    workday: require('./Workday.png').default,
+}
+
+export default imageFiles;
