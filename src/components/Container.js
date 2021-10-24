6737bd40797bc6389a6ea13faddda32e051b6a20
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 12:01:19 +1000

    added props to pass to Portfolio.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -12,25 +12,46 @@ export default function Container() {
     const publicProjects = [
         {
             title: 'Barter Place',
-            img: "",
-            github: "",
-            website: "",
+            img: "barter",
+            github: "https://github.com/Dinimar-Fishboi/Project_2_team3",
+            website: "https://project2t3.herokuapp.com/",
             id: 1,
         },
         {
             title: "Help I'm Bored",
-            img: "",
-            github: "",
-            website: "",
+            img: "bored",
+            github: "https://github.com/rajnidua/project-one",
+            website: "https://rajnidua.github.io/project-one/",
             id: 2,
         },
         {
             title: 'Note Taker',
-            img: "",
-            github: "",
-            website: "",
+            img: "note",
+            github: "https://github.com/Dinimar-Fishboi/NoteTaker",
+            website: "https://eleven-note-take.herokuapp.com/",
             id: 3,
         },
+        {
+            title: 'Budget Tracker',
+            img: "budget",
+            github: "https://github.com/Dinimar-Fishboi/Budget_Tracker",
+            website: "https://boiling-brook-13121.herokuapp.com",
+            id: 4,
+        },
+        {
+            title: 'Password Generator',
+            img: "password",
+            github: "https://github.com/Dinimar-Fishboi/PasswordGenerator",
+            website: "https://dinimar-fishboi.github.io/PasswordGenerator/",
+            id: 5,
+        },
+        {
+            title: 'Work Day Scheduler',
+            img: "workday",
+            github: "https://github.com/Dinimar-Fishboi/Work-Day-Scheduler",
+            website: "https://dinimar-fishboi.github.io/Work-Day-Scheduler/",
+            id: 6,
+        }
         
     ]
   
