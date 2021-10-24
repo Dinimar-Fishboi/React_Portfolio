b5acf806b573031bebd3312f51f9e0010c3b77da
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 21:54:04 +1000

    added skill list

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,16 +3,46 @@ import resumePdf from './resumeFiles/dev.pdf'
 
 export default function Resume() {
 
-   
+   const frontEnd = [
+     'HTML', 'CSS3', 'JavaScript', 'JQuery', 'Bootstrap', 'React'
+   ]
+
+   const backEnd = [
+     "API's", "Node", "Express", "MySql, Sequelize", "MongoDB, Mongoose", "REST", "GraphQL"
+   ]
 
   return (
     <div className="border">
       <h1>Resume</h1>
-      
       <p>
         Please download my <a download={resumePdf} href={resumePdf}>Resume</a>
       </p>
-     
+      <div>
+        <h3> Front-end</h3>
+        <ul>
+            {frontEnd.map(
+              (item) => {
+                return (
+                  <li>
+                    {item}
+                  </li>
+                )
+              }
+            )}
+        </ul>
+        <h3> Back-end</h3>
+        <ul>
+            {backEnd.map(
+              (item) => {
+                return (
+                  <li>
+                    {item}
+                  </li>
+                )
+              }
+            )}
+        </ul>
+      </div>
     </div>
   );
 }
