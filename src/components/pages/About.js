aecab394a6e4b67b385ec616bfa6c691afd96fb8
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 20:47:09 +1000

    updated content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -15,9 +15,9 @@ export default function About() {
       flexDirection: 'column',
       width: '75%'
     },
-    ul: {
-      listStyleType: 'none'
-    },
+    // ul: {
+    //   listStyleType: 'none'
+    // },
     img: {
       width: '200px',
       height: 'auto', 
@@ -32,21 +32,21 @@ export default function About() {
           <h3>
             Dev. Fish is a Full Stack Web Developer with a background in E-Learning Development, Relational Aesthetics and Customer Service Systems. 
           </h3>
+          <h4>
+            They are...
+          </h4>
           <ul style={styles.ul}>
             <li>
-                They are motivated to provide fluid and instinctual UI and UX experiences for users to have simple, instinctual access to online services.
+                Motivated to provide fluid and instinctual UI and UX experiences for users to have simple, instinctual access to online services.
             </li>
             <li>
-                They value positive team environments that are actively inclusive, open and consistently consider how to improve the welfare of their users.
+                Positive and value team environments that are actively inclusive, open and consistently consider how to improve the welfare of their users.
             </li>
             <li>
-                They have experience in HTML5, CSS, JavaScript, Bootstrap, jQuery, MySql, and MERN systems.
+                Experienced in HTML5, CSS, JavaScript, Bootstrap, jQuery, MySql, and MERN systems.
             </li>
             <li>
-                They aim to bring strong collaborative energy and concept-driven execution paired with agile development strategies to all projects they are involved in. 
-            </li>
-            <li>
-                They have a Certificate in Full Stack Web Development and a First Class Honours Degree in Visual Art.
+                Always bringing a strong collaborative energy with concept-driven execution paired and agile development strategies to all projects they are involved in. 
             </li>
           </ul>
           </Text>
