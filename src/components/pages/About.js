13c03ec64639e01e4acbc1f0731a5479afe8138b
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 16:45:30 +1000

    content and styling added to About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,57 @@
 import React from 'react';
+import { Text, View } from "react-native";
 import avatar from './avatar.png'
 
 
 export default function About() {
+
+  const styles= {
+    container: {
+      flex : 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    text: {
+      flexDirection: 'column',
+      width: '75%'
+    },
+    ul: {
+      listStyleType: 'none'
+    },
+    img: {
+      width: '200px',
+      height: 'auto', 
+    },
+  }
+
   return (
     <div className="border">
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-      <img src={avatar} alt='avatar'/>
-
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          <h1>About Page</h1>
+          <h3>
+            Dev. Fish is a Full Stack Web Developer with a background in E-Learning Development, Relational Aesthetics and Customer Service Systems. 
+          </h3>
+          <ul style={styles.ul}>
+            <li>
+                They are motivated to provide fluid and instinctual UI and UX experiences for users to have simple, instinctual access to online services.
+            </li>
+            <li>
+                They value positive team environments that are actively inclusive, open and consistently consider how to improve the welfare of their users.
+            </li>
+            <li>
+                They have experience in HTML5, CSS, JavaScript, Bootstrap, jQuery, MySql, and MERN systems.
+            </li>
+            <li>
+                They aim to bring strong collaborative energy and concept-driven execution paired with agile development strategies to all projects they are involved in. 
+            </li>
+            <li>
+                They have a Certificate in Full Stack Web Development and a First Class Honours Degree in Visual Art.
+            </li>
+          </ul>
+          </Text>
+        <img src={avatar} style={styles.img} alt='avatar'/>
+      </View>
     </div>
   );
 }
