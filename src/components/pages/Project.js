ff6da3bf2f6387a9dba80ceb477fb312d23fd1de
Dinimar Fish <tayla@tallisman.com.au>
2021-10-24 16:08:37 +1000

    resolve styling for Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,23 +1,70 @@
 import React from 'react';
+import { StyleSheet, Text, View } from "react-native";
 import { FaGithub } from "react-icons/fa";
 
 export default function Project({projects}) {
-    const styles = {
-        text: {
-            display: 'flex',
-            color : 'black',
-            width: "40%",
-            height: "40%",
-            alignItems: "center",
-            // flexDirectioon: "column",
-            justifyContent: 'flex-end',
-            flexWrap: "wrap",
+    // const styles = {
+    //     text: {
+    //         flex: 1,
+    //         color : 'black',
+    //         width: "40%",
+    //         height: "40%",
+    //         alignItems: "center",
+    //         // flexDirectioon: "column",
+    //         justifyContent: 'flex-end',
+    //         flexWrap: "wrap",
+    //     },
+    // }
+
+    const styles = StyleSheet.create({
+        container: {
+           flex: 2,
+           flexDirection: 'row',
+           flexWrap: 'wrap',
         },
-    }
+        card: {
+            color: 'black',
+            flexWrap: 'wrap',
+            minWidth: '40%',
+            flex: 2,
+            margin: '2%',
+        },
+        text: {
+            justifyContent: "space-around",
+            alignItems: "flex-end",
+            flexDirection: "column",
+            height: "100%",
+        }
+    })
+    
 
     return (
         <>
-             {projects.map(
+            <View style={styles.container}>
+            {projects.map(
+                (item) =>   {
+                return (
+                    <View className="card" style={styles.card} key={item.id}>
+                    <img src={item.img} className="card-img img" alt={item.img} />
+                    <div className="card-img-overlay">
+                        <Text style={styles.text}> 
+                            <div className="card-text" style={{display: "flex"}}>
+                                <section className="subtitle">
+                                <h5 className="card-title" ><a href={item.website}>{item.title}</a></h5> 
+                                <a href={item.github}>
+                                    <FaGithub  style={{fontSize: '40px'}} />
+                                </a>
+                                </section>
+                            </div>
+                        </Text> 
+                    </div>
+                    </View>
+                      )
+                  }
+              )
+            }
+            </View>
+             {/* {projects.map(
                 (item) =>   {
                 return (
                     <div className="card" style={styles.text} key={item.id}>
@@ -33,7 +80,7 @@ export default function Project({projects}) {
                       )
                   }
               )
-            }
+            } */}
         </>
     )
 }
