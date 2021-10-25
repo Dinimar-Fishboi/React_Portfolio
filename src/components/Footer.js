13de9aab443343be526814c6b9221aca4f7579d8
Dinimar Fish <tayla@tallisman.com.au>
2021-10-25 17:30:25 +1000

    added icons and styling to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { StyleSheet, View } from "react-native";
+import { FaGithub, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+
 
 export default function Footer () {
+
+  const styles = StyleSheet.create ({
+    space: {
+      flexDirection: "row",
+      width: "100%",
+      justifyContent: "center",
+      position:'absolue',  
+    }
+  })
+
     return (
-        <div className="footer">
-          <p>
-            Footer = add links and icons for footer later
-          </p>
-        </div>
+      <>
+        <View className="footer" style={styles.space}>
+          <a href="https://github.com/Dinimar-Fishboi">
+            <FaGithub  style={{fontSize: '40px', margin: '1rem'}} />
+         </a>
+         <a href="https://www.linkedin.com/in/taytallis/">
+            <FaLinkedin  style={{fontSize: '40px', margin: '1rem'}} />
+         </a>
+         <a href="https://stackoverflow.com">
+            <FaStackOverflow  style={{fontSize: '40px', margin: '1rem'}} />
+         </a>
+        </View>
+      </>
       );
 }
